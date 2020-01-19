1ed0e5dad15adfeca238f73e9e3ae29253db5c34
Uvindu Shehan Perera <44149708+uvinduperera@users.noreply.github.com>
2020-01-19 16:02:40 +0530

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,91 @@
+<?php
+session_start();
+
+$message = ''; 
+$username = $_POST['username'];
+$password = $_POST['password'];
+$gender = $_POST['gender'];
+$email = $_POST['email'];
+$phoneCode = $_POST['phoneCode'];
+$phone = $_POST['phone'];
+if (!empty($username) || !empty($password) || !empty($gender) || !empty($email) || !empty($phoneCode) || !empty($phone)) {
+ $host = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbname = "senuweb";
+    //create connection
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+    if (mysqli_connect_error()) {
+     die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+    } else {
+     $SELECT = "SELECT email From register Where email = ? Limit 1";
+     $INSERT = "INSERT Into register (username, password, gender, email, phoneCode, phone) values(?, ?, ?, ?, ?, ?)";
+     //Prepare statement
+     $stmt = $conn->prepare($SELECT);
+     $stmt->bind_param("s", $email);
+     $stmt->execute();
+     $stmt->bind_result($email);
+     $stmt->store_result();
+     $rnum = $stmt->num_rows;
+     if ($rnum==0) {
+      $stmt->close();
+      $stmt = $conn->prepare($INSERT);
+      $stmt->bind_param("ssssii", $username, $password, $gender, $email, $phoneCode, $phone);
+      $stmt->execute();
+      echo "New record inserted sucessfully";
+     } else {
+      echo "Someone already register using this email";
+     }
+     $stmt->close();
+     $conn->close();
+    }
+} else {
+ echo "All field are required";
+ die();
+}
+if (isset($_POST['uploadBtn']) && $_POST['uploadBtn'] == 'Upload')
+{
+  if (isset($_FILES['uploadedFile']) && $_FILES['uploadedFile']['error'] === UPLOAD_ERR_OK)
+  {
+    // get details of the uploaded file
+    $fileTmpPath = $_FILES['uploadedFile']['tmp_name'];
+    $fileName = $_FILES['uploadedFile']['name'];
+    $fileSize = $_FILES['uploadedFile']['size'];
+    $fileType = $_FILES['uploadedFile']['type'];
+    $fileNameCmps = explode(".", $fileName);
+    $fileExtension = strtolower(end($fileNameCmps));
+
+    // sanitize file-name
+    $newFileName = md5(time() . $fileName) . '.' . $fileExtension;
+
+    // check if file has one of the following extensions
+    $allowedfileExtensions = array('jpg', 'gif', 'png', 'zip', 'txt', 'xls', 'doc', 'pdf');
+
+    if (in_array($fileExtension, $allowedfileExtensions))
+    {
+      // directory in which the uploaded file will be moved
+      $uploadFileDir = './uploaded_files/';
+      $dest_path = $uploadFileDir . $newFileName;
+
+      if(move_uploaded_file($fileTmpPath, $dest_path)) 
+      {
+        $message ='File is successfully uploaded.';
+      }
+      else 
+      {
+        $message = 'There was some error moving the file to upload directory. Please make sure the upload directory is writable by web server.';
+      }
+    }
+    else
+    {
+      $message = 'Upload failed. Allowed file types: ' . implode(',', $allowedfileExtensions);
+    }
+  }
+  else
+  {
+    $message = 'There is some error in the file upload. Please check the following error.<br>';
+    $message .= 'Error:' . $_FILES['uploadedFile']['error'];
+  }
+}
+$_SESSION['message'] = $message;
+header("Location: index.php");
