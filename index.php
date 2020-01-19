cfcb5c507980233630d358be4d1650001f206cda
Uvindu Shehan Perera <44149708+uvinduperera@users.noreply.github.com>
2020-01-19 19:24:58 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,51 +1,64 @@
-<?php
-session_start(); 
-?>
+
 <!DOCTYPE html>
 <html>
 <head>
   <title>PHP File Upload</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
+<div class="container">
 <form action="upload.php" method="POST" enctype="multipart/form-data">
   <table>
-   <tr>
-    <td>Name :</td>
-    <td><input type="text" name="username" required></td>
+  <div class="form-group">
+   
+   <label for="firstname">First Name:</label>
+    <input type="text" name="username" required class="form-control">
+   
+   </div>
+   <div class="form-group">
+    <label for="lastname">Last Name :</label>
+    <input type="text" name="password" required class="form-control">
+</div>
+  
+<div class="form-group">
+  
+    <label for="email">Email :</label>
+    <input type="email" name="email" required class="form-control">
+    
+</div>
+   <div class="form-group">
+    <label for="phoneno">Phone no :</label>
+    <td>
+     <select name="phoneCode" required class="form-control">
+       <h2>Contact No</h2>
+      <option selected hidden value="">Select Code</option>
+      <option value="977">011</option>
+      <option value="978">034</option>
+      <option value="979">035</option>
+      <option value="973">077</option>
+      <option value="972">071</option>
+      <option value="974">078</option>
+     </select>
+     <input type="phone" name="phone" required class="form-control">
+    </td>
    </tr>
-   <tr>
-    <td>Password :</td>
-    <td><input type="password" name="password" required></td>
-   </tr>
-   <tr>
+</div>
+
+<div class="form-group">
     <td>Gender :</td>
     <td>
      <input type="radio" name="gender" value="m" required>Male
      <input type="radio" name="gender" value="f" required>Female
     </td>
    </tr>
-   <tr>
-    <td>Email :</td>
-    <td><input type="email" name="email" required></td>
-   </tr> 
-   <tr>
-    <td>Phone no :</td>
-    <td>
-     <select name="phoneCode" required>
-      <option selected hidden value="">Select Code</option>
-      <option value="977">977</option>
-      <option value="978">978</option>
-      <option value="979">979</option>
-      <option value="973">973</option>
-      <option value="972">972</option>
-      <option value="974">974</option>
-     </select>
-     <input type="phone" name="phone" required>
-    </td>
-   </tr>
+</div>
    <tr>
      <td>
-     <span>Upload a File:</span>
+     <span>Upload Your CV:</span>
       <input type="file" name="uploadedFile" />
 
      </td>
@@ -65,5 +78,6 @@ session_start();
 
     
   </form>
+  </div>
 </body>
 </html>
